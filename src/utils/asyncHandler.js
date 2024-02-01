a30bdf61bfaf13a8d727cae50cd00fef18231529
Sonali <sonalimoharana133@gmail.com>
2024-02-02 00:19:01 +0530

    Added utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHnadler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHnadler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = (fn)=> async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch(error){
+//         res.status(err.code || 500).json({
+//            success: false,
+//            message: err.message
+//         })
+//     }
+// }
